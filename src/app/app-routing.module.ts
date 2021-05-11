bed6c1a18e4ab4164a35644f8d5c5c0243d45cae
ronal <ronal101012@gmail.com>
2021-05-11 14:38:14 -0500

    start pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RecoverComponent } from './recover/recover.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'recover', component: RecoverComponent },
+  { path: '**', component: PageNotFoundComponent },
+
+  { path: '', redirectTo:'/login', pathMatch:'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ContactComponent } from './contact/contact.component';
+import { RecoverComponent } from './recover/recover.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    ContactComponent,
+    RecoverComponent,
+    PageNotFoundComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
